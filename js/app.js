008a0e168edfc3bc066ce2903dc9236f103fb29d
Farhan <linuxossysdev@gmail.com>
2021-09-05 11:40:41 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -7,9 +7,11 @@ const setInnerText = (id, text) => {
 const searchTemperature= () => {
     const city= document.getElementById("city-name").value;
     const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APT_KEY}&units=metric`;
-    
+   
     fetch(url).then(res => res.json())
     .then( data => displayTemperature(data));
+    
+    document.getElementById("city-name").value='';
 }
    
 
@@ -25,5 +27,6 @@ const displayTemperature = temperature => {
     const url= `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
     const imgIcon= document.getElementById("weather-icon");
     imgIcon.setAttribute('src', url)
+
 
 } 
